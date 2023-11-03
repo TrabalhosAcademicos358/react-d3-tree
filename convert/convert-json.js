6fb8aa093dbbd4999ee3145d1c09c5e54e468a51
Pedro <pedrojuniorifpb@gmail.com>
2023-11-03 09:55:20 -0300

    Conversão funcionando com sucesso

--- convert/convert-json.js
+++ convert/convert-json.js
@@ -1,40 +1,42 @@
-import json from './results.json';
+import * as jsonFile from './results.json' assert { type: "json" };
+import fs from 'fs';
 
+const json = jsonFile.default;
 let jsonResult = {};
+
+const separatePath = '->';
 
 const createObj = (name, attributes) => {
   return { name, children: [], attributes };
 };
 
-const getValueFromObj = (value, obj) => {
-    const 
-}
-
 const addChildrenObj = (key, obj) => {
-  let objCurrent = jsonResult.root;
-  let pathListObjInJson = ""
-  let cont = 0
-  const pathTestCase = key.split('->');
+  let objCurrent = jsonResult;
+  const pathTestCase = key.split(separatePath);
   pathTestCase.shift();
+  pathTestCase.pop();
 
   pathTestCase.forEach(keyPath => {
-    objCurrent = objCurrent.children.find(item => item.name === keyPath)
-    if (objCurrent)
+    objCurrent = objCurrent.children.find(item => item.name.includes(keyPath))
   })
+
+  const newObj = createObj(key, obj)
+  objCurrent.children.push(newObj)
+  return newObj
 };
 
-const linksForNodesGroup = Object.keys(json)
-  .map(key => {
-    const pathTestCase = key.split('->');
+Object.keys(json)
+  .forEach(key => {
+    const pathTestCase = key.split(separatePath);
     if (pathTestCase.length > 1) {
-      pathTestCase.pop();
-      const stringIdNodeSource = pathTestCase.join('->');
-
-      return { source: stringIdNodeSource, target: key, value: 4 };
+      // pathTestCase.pop();
+      // const stringIdNodeSource = pathTestCase.join(separatePath);
+      addChildrenObj(key, json[key])
+      // return { source: stringIdNodeSource, target: key, value: 4 };
     } else {
       const newObj = createObj(key, json[key]);
-      jsonResult = { ...jsonResult, newObj };
+      jsonResult = { ...jsonResult, ...newObj };
     }
-    return null;
   })
-  .filter(item => item !== null);
+
+fs.writeFileSync('/home/pedro/GitHub/outros/react-d3-tree/demo/src/examples/results.json', JSON.stringify(jsonResult))
